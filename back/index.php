6855ddf9ea007dfd30f6913247e817e012a2af6d
YuFen <yufenhuangtaiwan.com>
2022-02-18 03:30:48 +0800

    建立所需的後台頁面

--- back/index.php
+++ back/index.php
@@ -21,7 +21,7 @@
         <!-- Sidebar -->
         <div class="flex-grow-2 bg-white">
             <div class="list-group list-group-flush my-3">
-                <a href="./dashboard.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="far fa-eye me-2"></i>Theme</a>
+                <a href="./dashboard.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="far fa-eye me-2"></i>Scheme</a>
                 <a href="./about.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="far fa-address-card me-2"></i>About</a>
                 <a href="./work-exp.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="far fa-building me-2"></i>WorkExp.</a>
                 <a href="./gallery.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-project-diagram me-2"></i>Gallery</a>
