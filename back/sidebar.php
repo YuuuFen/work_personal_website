f45f2a7e2118bf1c7a9dee24e3a34c5007fd7206
YuuuFen <yufenhuangtaiwan@gmail.com>
2022-02-18 08:54:00 +0800

    新增後台各頁面標題

--- back/sidebar.php
+++ back/sidebar.php
@@ -1,7 +1,7 @@
 <!-- Sidebar -->
 <div class="flex-grow-2 bg-white">
     <div class="list-group list-group-flush my-3">
-        <a href="./visitor.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="far fa-eye me-2"></i>Visitors</a>
+        <a href="./visitor.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-users me-2"></i>Visitors</a>
         <a href="./scheme.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="far fa-eye me-2"></i>Scheme</a>
         <a href="./about.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="far fa-address-card me-2"></i>About</a>
         <a href="./work-exp.php" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="far fa-building me-2"></i>WorkExp.</a>
