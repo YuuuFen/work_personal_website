b67b84f1fcea9b3f307a246bf3baaa6631f679de
YuFen <yufenhuangtaiwan.com>
2022-04-06 15:55:10 +0800

    ABOUT: 功能完成; 增加 bootstrap4 到 back.php

--- api/edit_about.php
+++ api/edit_about.php
@@ -1,13 +1,19 @@
 <?php
 include_once "../base.php";
 
-foreach ($_POST['id'] as $key => $id) {
-    //更新
-    $data = $About->find($id);
-    $data['name_en'] = $_POST['name_en'][$key];
-    $data['email'] = $_POST['email'][$key];
-    dd($data);
+//把資料用id這個欄位的值取出來
+$data = $About->find($_POST['id']);
+
+//把$_POST中的欄位名和值分開成key和val兩個變數
+foreach ($_POST as $key => $val) {
+    // 更新
+    // 排除id欄位
+    if ($key != 'id') {
+        //將每個表單傳來的值依照對應的欄位名稱寫入到資料中
+        $data[$key] = $val;
+    }
+    // dd($data);
     $About->save($data);
 }
 
-// to("../back.php?do=about");
+to("../back.php?do=about");
--- back/about.php
+++ back/about.php
@@ -2,63 +2,57 @@
 $row = $About->all()[0];
 ?>
 
-<fieldset>
-    <legend>
-        <h2>ABOUT</h2>
-    </legend>
+<div class="container">
+    <h2>ABOUT</h2>
+    <form action="../api/edit_about.php" method="post">
+        <table>
+            <tr>
+                <td><input type="hidden" name="id" id="id" value="<?= $row['id'] ?>"></td>
+            </tr>
+            <tr>
+                <td>中文名</td>
+                <td><input type="text" class="" name="name_cn" id="name_cn" value="<?= $row['name_cn'] ?>" disabled></td>
+            </tr>
+            <tr>
+                <td>英文名</td>
+                <td><input type="text" class="w-100" name="name_en" id="name_en" value="<?= $row['name_en'] ?>"></td>
+            </tr>
+            <tr>
+                <td>信箱</td>
+                <td><input type="text" class="w-100" name="email" id="email" value="<?= $row['email'] ?>"></td>
+            </tr>
+            <tr>
+                <td>GitHub</td>
+                <td><input type="text" class="w-100" name="github" id="github" value="<?= $row['github'] ?>"></td>
+            </tr>
+            <tr>
+                <td>居住地英文</td>
+                <td><input type="text" class="w-100" name="en_location" id="en_location" value="<?= $row['en_location'] ?>"></td>
+            </tr>
+            <tr>
+                <td>居住地中文</td>
+                <td><input type="text" class="w-100" name="cn_location" id="cn_location" value="<?= $row['cn_location'] ?>"></td>
+            </tr>
+            <tr>
+                <td>英文自我介紹</td>
+                <td><textarea name="en_brief" class="w-100" id="en_brief" cols="30" rows="10"><?= $row['en_brief'] ?></textarea></td>
+            </tr>
+            <tr>
+                <td>中文自我介紹</td>
+                <td><textarea name="cn_brief" class="w-100" id="cn_brief" cols="30" rows="10"><?= $row['cn_brief'] ?></textarea></td>
+            </tr>
+            <tr>
+                <td>技能</td>
+                <td><textarea name="skill" class="w-100" id="skill" cols="30" rows="10"><?= $row['skill'] ?></textarea></td>
+            </tr>
+        </table>
+        <div>
+            <input class="btn btn-sm btn-info d-inline-block" type="submit" value="確定">
+            <input class="btn btn-sm btn-warning" type="reset" value="重置">
+        </div>
+    </form>
+</div>
 
-    <div>
-        <form action="../api/edit_about.php" method="post">
-            <table>
-                <!-- <tr>
-                    <td>ID</td>
-                    <td><input type="text" class="w-100" name="id" id="id" value="<?= $row['id'] ?>" disabled></td>
-                </tr> -->
-                <tr>
-                    <td>中文名</td>
-                    <td><input type="text" class="w-100" name="name_cn" id="name_cn" value="<?= $row['name_cn'] ?>" disabled></td>
-                </tr>
-                <tr>
-                    <td>英文名</td>
-                    <td><input type="text" class="w-100" name="name_en" id="name_en" value="<?= $row['name_en'] ?>"></td>
-                </tr>
-                <tr>
-                    <td>信箱</td>
-                    <td><input type="text" class="w-100" name="email" id="email" value="<?= $row['email'] ?>"></td>
-                </tr>
-                <tr>
-                    <td>GitHub</td>
-                    <td><input type="text" class="w-100" name="github" id="github" value="<?= $row['github'] ?>"></td>
-                </tr>
-                <tr>
-                    <td>居住地英文</td>
-                    <td><input type="text" class="w-100" name="en_location" id="en_location" value="<?= $row['en_location'] ?>"></td>
-                </tr>
-                <tr>
-                    <td>居住地中文</td>
-                    <td><input type="text" class="w-100" name="cn_location" id="cn_location" value="<?= $row['cn_location'] ?>"></td>
-                </tr>
-                <tr>
-                    <td>英文自我介紹</td>
-                    <td><textarea name="en_brief" class="w-100" id="en_brief" cols="30" rows="10"><?= $row['en_brief'] ?></textarea></td>
-                </tr>
-                <tr>
-                    <td>中文自我介紹</td>
-                    <td><textarea name="cn_brief" class="w-100" id="cn_brief" cols="30" rows="10"><?= $row['cn_brief'] ?></textarea></td>
-                </tr>
-                <tr>
-                    <td>技能</td>
-                    <td><textarea name="skill" class="w-100" id="skill" cols="30" rows="10"><?= $row['skill'] ?></textarea></td>
-                </tr>
-            </table>
-            <div>
-                <input class="btn btn-sm btn-info d-inline-block" type="submit" value="確定">
-                <input class="btn btn-sm btn-warning" type="reset" value="重置">
-            </div>
-        </form>
-    </div>
-
-</fieldset>
 
 <!-- <div class="row mb-3">
     <div class="col-2">中文名：</div>
