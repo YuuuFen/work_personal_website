89b3523df607e795454656ce08a1fba1a75e2aac
YuFen <yufenhuangtaiwan.com>
2022-04-06 14:54:09 +0800

    ABOUT: API檔報錯

--- api/edit_about.php
+++ api/edit_about.php
@@ -6,10 +6,8 @@ foreach ($_POST['id'] as $key => $id) {
     $data = $About->find($id);
     $data['name_en'] = $_POST['name_en'][$key];
     $data['email'] = $_POST['email'][$key];
-    // $data['sh'] = (isset($_POST['sh']) && in_array($id, $_POST['sh'])) ? 1 : 0;
     dd($data);
     $About->save($data);
 }
-
 
 // to("../back.php?do=about");
--- front/about.php
+++ front/about.php
@@ -1,3 +1,7 @@
+<?php
+$row = $About->all()[0];
+?>
+
 <!-- About -->
 <div id="about">
     <!-- pro pic & info -->
@@ -7,11 +11,11 @@
         </div>
         <div id="info">
             <ul>
-                <li>Yu-Fen Huang</li>
-                <li>Taipei City, Taiwan</li>
+                <li><?= $row['name_en'] ?></li>
+                <li><?= $row['en_location'] ?></li>
+                <li><?= $row['email'] ?></li>
                 <li>
-                    <a href="https://github.com/YuuuFen"><i class="fab fa-github" style="font-size: 2em;"></i></a>
-                    <a href="https://www.linkedin.com/in/yu-fen-huang-886a6215a/"><i class="fab fa-linkedin" style="font-size: 2em;"></i></a>
+                    <a href="<?= $row['github'] ?>"><i class="fab fa-github" style="font-size: 2em;"></i></a>
                 </li>
             </ul>
         </div>
@@ -23,7 +27,7 @@
             <h2>About Me</h2>
         </div>
         <div id="intro">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae aliquam at nemo voluptas neque quis soluta esse incidunt, dolore voluptate minus? Quibusdam fuga accusantium, accusamus eveniet, porro perspiciatis nostrum enim adipisci voluptatum molestiae magni sint aut cumque non aliquid, voluptates dolore! Non corrupti laboriosam modi labore ad dolorum laudantium dignissimos.
+            <p><?= $row['en_brief'] ?></p>
         </div>
     </div>
     <!-- intro end -->
