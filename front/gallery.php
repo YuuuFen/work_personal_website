fc46688b10f3347094835bb888d85a4f077c5dc5
YuFen <yufenhuangtaiwan.com>
2022-04-11 23:19:47 +0800

    GALLERY: 連資料庫

--- front/gallery.php
+++ front/gallery.php
@@ -1,38 +1,23 @@
-<!-- Gallery start -->
+<?php
+$row = $Works->all();
+?>
+
 <div id="gallery">
     <div>
         <h2>Gallery</h2>
     </div>
-    <div class="card-body">
-        <div class="card-img"><img src="https://fakeimg.pl/320x180/ffeecc/" alt=""></div>
-        <div>
-            <ul>
-                <li>one sentence</li>
-            </ul>
-        </div>
-    </div>
-    <div class="card-body">
-        <div class="card-img"><img src="https://fakeimg.pl/320x180/ffeecc/" alt=""></div>
-        <div>
-            <ul>
-                <li>
-                    <h3 class="card-title">Calender</h3>
-                </li>
-                <li>one sentence</li>
-            </ul>
-        </div>
-    </div>
-    <div class="card-body">
-        <div class="card-img"><img src="https://fakeimg.pl/320x180/ffeecc/" alt=""></div>
-        <div>
-            <ul>
-                <li>
-                    <h3 class="card-title">Calender</h3>
-                </li>
-                <li>one sentence</li>
-            </ul>
-        </div>
-    </div>
+    <?php
+    foreach ($row as $value) {
+        echo "<div class='card_img'>";
+        echo "<img src='../upload/{$value['img_1']}' style='max-height: 200px;'>";
+        echo "</div>";
+        echo "<div class='card_body'>";
+        echo "<h3>{$value['title']}</h3>";
+        echo "<li>{$value['intro']}</li>";
+        echo "<li>主要技術：{$value['tag']}</li>";
+        echo "</div>";
+    }
+    ?>
     <div class="card-body">
         <div class="card-img"><img src="https://fakeimg.pl/320x180/ffeecc/" alt=""></div>
         <div>
